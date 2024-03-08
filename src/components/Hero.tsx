eb7a02bcb3e02b4557c41f65c11b831eff8029cb
coderreco <dev@coderre.co>
2024-03-08 14:05:39 -0800

    Remove YouTube link from Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ export function Hero() {
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="/register">Get started</Button>
         <Button
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          href=""
           variant="outline"
         >
           <svg
